64938a3661dc6d02557c06864c14e5484fb5279f
Alexander <pchp250g8@tutamail.com>
2026-02-28 22:13:56 +0300

    Update Task-4-1.py

--- Task-4-1.py
+++ Task-4-1.py
@@ -1,14 +1,13 @@
 from robot import *
 
 while(is_free_right() or is_free_left() or is_free_down()):
-    while(is_free_right()):
+    while(is_free_right() and is_wall_down()):
         paint()
         move_right()
-    paint()
-    move_down()
-    while(is_free_left()):
+    while(is_free_left() and is_wall_down()):
         paint()
         move_left()
-    paint()
-    move_down()
+    while is_free_down():
+        paint()
+        move_down()
 paint()
